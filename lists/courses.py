377c49d12240c0735bd8cbc9392abd3399de2465
Nikolai Angelov <nikolaiangelovnix@gmail.com>
2023-01-25 22:59:38 +0200

    added list practice problems

---
+++ lists/courses.py
@@ -0,0 +1,6 @@
+lines_number = int(input())
+courses_list = []
+for _ in range(lines_number):
+    current_course = input()
+    courses_list.append(current_course)
+print(courses_list)
---
+++ lists/list_statistics.py
@@ -0,0 +1,15 @@
+lines_number = int(input())
+positive_nums = []
+negative_nums = []
+
+for _ in range(lines_number):
+    current_num = int(input())
+    if current_num >= 0:
+        positive_nums.append(current_num)
+    else:
+        negative_nums.append(current_num)
+
+print(positive_nums)
+print(negative_nums)
+print(f"Count of positives: {len(positive_nums)}")
+print(f"Sum of negatives: {sum(negative_nums)}")
